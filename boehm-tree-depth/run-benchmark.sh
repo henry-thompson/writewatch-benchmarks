b9a0140ab74f1d2443b4c15a9eb1889d8849c389
Henry Thompson <hello@henrythompson.me>
2018-03-30 13:59:15 +0100

    We want to keep the unaggregated data in case its useful

--- boehm-tree-depth/run-benchmark.sh
+++ boehm-tree-depth/run-benchmark.sh
@@ -6,24 +6,24 @@
 benchmark()
 {
     echo "kStretchTreeDepth	LongLivedTreeDepth	kArraySize	kMinTreeDepth	kMaxTreeDepth	GC Itertations	Heap Size	Time Elapsed\
-" > "unaggregated.temp"
+" > "$1.unaggregated.tsv"
 
     for ((i=1;i<=30;i++));
     do
         for ((kLongLivedTreeDepth=16;kLongLivedTreeDepth<=25;kLongLivedTreeDepth++));
         do
             echo "Iter $i: kLongLivedTreeDepth=$kLongLivedTreeDepth"
-            rtprio 0 ../boehm/benchmark 19 "$kLongLivedTreeDepth" >> "unaggregated.temp"
+            rtprio 0 ../boehm/benchmark 19 "$kLongLivedTreeDepth" >> "$1.unaggregated.temp"
         done
     done
 
-    python2 aggregate.py > "$1"
+    python2 aggregate.py > "$1.tsv"
 }
 
 # Build and install the original GC
 ../boehm/install.sh -original -build-gc
-benchmark "result-baseline.tsv"
+benchmark "result-baseline"
 
 # Now for the new version
 ../boehm/install.sh -buffer 16 -build-gc
-benchmark "result-writewatch.tsv"
+benchmark "result-writewatch"
