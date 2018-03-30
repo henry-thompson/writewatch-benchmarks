d1b8a04c45a888eafe04884b0f13fcce94f7c5c7
Henry Thompson <hello@henrythompson.me>
2018-03-30 15:00:11 +0100

    Allow user to specify short-lived-tree depth

--- boehm-tree-depth/run-benchmark.sh
+++ boehm-tree-depth/run-benchmark.sh
@@ -3,6 +3,9 @@
 # Keeping the short-lived tree size fixed, loop over the long-lived tree
 # sizes.
 
+# Options:
+#    -short-lived-tree N             Specifies use short-lived tree depth N
+
 benchmark()
 {
     echo "kStretchTreeDepth	LongLivedTreeDepth	kArraySize	kMinTreeDepth	kMaxTreeDepth	GC Itertations	Heap Size	Time Elapsed\
@@ -12,13 +15,18 @@ benchmark()
     do
         for ((kLongLivedTreeDepth=16;kLongLivedTreeDepth<=25;kLongLivedTreeDepth++));
         do
-            echo "Iter $i: kLongLivedTreeDepth=$kLongLivedTreeDepth"
-            rtprio 0 ../boehm/benchmark 19 "$kLongLivedTreeDepth" >> "$1.unaggregated.tsv"
+            echo "Iter $i | kLongLivedTreeDepth $kLongLivedTreeDepth | (kShortLivedTreeDepth $2)"
+            rtprio 0 ../boehm/benchmark "$2" "$kLongLivedTreeDepth" >> "$1.unaggregated.tsv"
         done
     done
 
     python2 aggregate.py "$1.unaggregated.tsv" > "$1.tsv"
 }
+
+shortLivedTreeDepth=21
+if [ "$1" = "-short-lived-tree" ]; then
+    shortLivedTreeDepth="$2"
+fi
 
 here="$PWD"
 cd ../boehm
@@ -31,10 +39,10 @@ chmod +x ./install.sh
 # Build and install the original GC
 ./install.sh -original -build-gc
 cd "$here"
-benchmark "result-baseline"
+benchmark "result-baseline" "$shortLivedTreeDepth"
 cd ../boehm
 
 # Now for the new version
 ./install.sh -buffer 16 -build-gc
 cd "$here"
-benchmark "result-writewatch"
+benchmark "result-writewatch" "$shortLivedTreeDepth"
